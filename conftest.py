20c062db2d7bdf686995dfe1ad76c93d94b8374e
LWJ12345 <liuweijialiu251@gmail>
2026-07-14 16:05:52 +0800

    san

--- conftest.py
+++ conftest.py
@@ -1,13 +1,26 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
 
 
 @pytest.fixture(scope='session')
 def driver():
-    #让所有test遵循此函数的前后置
-    d = webdriver.Chrome()
+    chrome_options = Options()
+    if os.getenv("CI") == "true":
+        chrome_options.add_argument("--headless=new")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--ignore-certificate-errors")
+        chrome_options.add_argument("--disable-web-security")
+        chrome_options.add_argument("--allow-running-insecure-content")
+
+    d = webdriver.Chrome(options=chrome_options)
     d.maximize_window()
-    d.implicitly_wait(2)  # 短的隐式等待
+    d.implicitly_wait(2)
 
     yield d
 
